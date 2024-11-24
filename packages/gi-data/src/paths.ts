2dc7df7717b889ee9f44e4edd8bb375a9309de84
Chris George <chrismathew98@gmail.com>
2024-11-23 21:37:06 -0500

    Fixed PROJECT_ROOT and added GIDATA_ROOT

--- packages/gi-data/src/paths.ts
+++ packages/gi-data/src/paths.ts
@@ -1,2 +1,3 @@
 import path from "path";
-export const PROJECT_ROOT = path.resolve(".");
+export const PROJECT_ROOT = path.resolve("..", "..", "packages", "gi-data"); // TODO: Find better way to get project root
+export const GIDATA_ROOT = path.join(PROJECT_ROOT, "AnimeGameData");
