5afb0cb8061546b8863b1ee311089a13b7e1b49e
Chris George <chrismathew98@gmail.com>
2024-11-24 14:08:09 -0500

    rename GI_DATA_ROOT to GI_DM_ROOT

--- packages/gi-data/src/paths.ts
+++ packages/gi-data/src/paths.ts
@@ -1,3 +1,3 @@
 import path from "path";
 export const PROJECT_ROOT = path.resolve("..", "..", "packages", "gi-data"); // TODO: Find better way to get project root
-export const GIDATA_ROOT = path.join(PROJECT_ROOT, "AnimeGameData");
+export const GI_DM_ROOT = path.join(PROJECT_ROOT, "AnimeGameData");
--- packages/gi-data/src/util.ts
+++ packages/gi-data/src/util.ts
@@ -1,8 +1,8 @@
 import { existsSync, readFileSync } from "fs";
-import { GIDATA_ROOT } from "./paths.js";
+import { GI_DM_ROOT } from "./paths.js";
 
 export function readDMJSON(path: string) {
-  const fullPath = `${GIDATA_ROOT}/${path}`;
+  const fullPath = `${GI_DM_ROOT}/${path}`;
   if (!existsSync(fullPath)) throw `File not found :${fullPath}`;
   return readFileSync(fullPath).toString();
 }
