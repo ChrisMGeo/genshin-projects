195b082472bf9d14a496f1973778276900c1bca1
Chris George <chrismathew98@gmail.com>
2024-12-12 23:12:26 -0500

    Map UTS locale codes for GI language codes

--- packages/gi-data/src/consts/languages.ts
+++ packages/gi-data/src/consts/languages.ts
@@ -17,3 +17,27 @@ export const giLanguages = [
 ] as const;
 
 export type GILanguage = (typeof giLanguages)[number];
+
+export const giLanguageToUTSMap = {
+  CHS: "zh-CN",
+  CHT: "zh-TW",
+  DE: "de",
+  EN: "en",
+  ES: "es",
+  FR: "fr",
+  ID: "id",
+  IT: "it",
+  JP: "ja",
+  KR: "ko",
+  PT: "pt",
+  RU: "ru",
+  TH: "th",
+  TR: "tr",
+  VI: "vi",
+} as const satisfies {
+  [inGameLocale in GILanguage]: string;
+};
+
+export const giUTSLanguages = Object.values(giLanguageToUTSMap);
+
+export type GIUTSLanguage = (typeof giLanguageToUTSMap)[GILanguage];
